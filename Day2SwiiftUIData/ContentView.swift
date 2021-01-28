b1298620a13dee2921acc9be7212b2f7e6dd02b1
Abdulmalik Muhammad <bawasuru@gmail.com>
2021-01-29 00:59:36 +0100

    Day 1 review code

--- Day2SwiiftUIData/ContentView.swift
+++ Day2SwiiftUIData/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      VStack {
+        Text("🎯🎯🎯\nPut the bulleye as close as you can to".uppercased())
+          .kerning(2)
+          .bold()
+          .font(.footnote)
+          .lineSpacing(4)
+          .multilineTextAlignment(.center)
+        
+        Text("89")
+          .kerning(-1.0)
+          .fontWeight(.black)
+          .font(.largeTitle)
+          .lineSpacing(42.0)
+        
+        HStack {
+          Text("1")
+            .bold()
+            .font(.title3)
+            .lineSpacing(21.0)
+          Slider(value: .constant(10), in: 1.0...100.0)
+          Text("100")
+            .bold()
+            .font(.title3)
+            .lineSpacing(21.0)
+        }
+        
+        Button(action: {
+          print("Button was tapped")
+        }) {
+          Text("Hit Me")
+        }
+      }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+      ContentView()
+      ContentView()
+          .previewLayout(.fixed(width: 538, height: 320))
     }
 }
